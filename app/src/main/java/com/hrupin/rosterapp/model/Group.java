eee4363f7cfdbcffc81f69b69ca7875a65a743b9
Igor Khrupin <igor@hrupin.com>
2016-05-04 22:01:55 +0200

    created Person and Group

---
+++ app/src/main/java/com/hrupin/rosterapp/model/Group.java
@@ -0,0 +1,11 @@
+package com.hrupin.rosterapp.model;
+
+import java.util.List;
+
+/**
+ * Created by Igor Khrupin www.hrupin.com on 5/4/16.
+ */
+public class Group {
+    private String groupName;
+    private List<Person> people;
+}
---
+++ app/src/main/java/com/hrupin/rosterapp/model/Person.java
@@ -0,0 +1,11 @@
+package com.hrupin.rosterapp.model;
+
+/**
+ * Created by Igor Khrupin www.hrupin.com on 5/4/16.
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String statusIcon;
+    private String statusMessage;
+}
