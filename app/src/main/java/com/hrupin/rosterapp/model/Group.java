21951e78647e5b5e9f32ebaaa8097d99afc55ebf
Igor Khrupin <igor@hrupin.com>
2016-05-04 22:18:43 +0200

    added Status and modified Group and Person

--- app/src/main/java/com/hrupin/rosterapp/model/Group.java
+++ app/src/main/java/com/hrupin/rosterapp/model/Group.java
@@ -1,11 +1,60 @@
 package com.hrupin.rosterapp.model;
 
-import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
 
 /**
  * Created by Igor Khrupin www.hrupin.com on 5/4/16.
  */
 public class Group {
+    @SerializedName("groupName")
     private String groupName;
-    private List<Person> people;
+
+    @SerializedName("people")
+    private ArrayList<Person> people;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(ArrayList<Person> people) {
+        this.people = people;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Group group = (Group) o;
+
+        if (groupName != null ? !groupName.equals(group.groupName) : group.groupName != null)
+            return false;
+        return people != null ? people.equals(group.people) : group.people == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = groupName != null ? groupName.hashCode() : 0;
+        result = 31 * result + (people != null ? people.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupName='" + groupName + '\'' +
+                ", people=" + people +
+                '}';
+    }
 }
--- app/src/main/java/com/hrupin/rosterapp/model/Person.java
+++ app/src/main/java/com/hrupin/rosterapp/model/Person.java
@@ -1,11 +1,87 @@
 package com.hrupin.rosterapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Igor Khrupin www.hrupin.com on 5/4/16.
  */
 public class Person {
+    @SerializedName("firstName")
     private String firstName;
+
+    @SerializedName("lastName")
     private String lastName;
-    private String statusIcon;
+
+    @SerializedName("statusIcon")
+    private Status status;
+
+    @SerializedName("statusMessage")
     private String statusMessage;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null)
+            return false;
+        if (lastName != null ? !lastName.equals(person.lastName) : person.lastName != null)
+            return false;
+        if (status != person.status) return false;
+        return statusMessage != null ? statusMessage.equals(person.statusMessage) : person.statusMessage == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (statusMessage != null ? statusMessage.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", status=" + status +
+                ", statusMessage='" + statusMessage + '\'' +
+                '}';
+    }
 }
